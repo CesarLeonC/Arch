d1cfa7881c217c897590de04e7e8a49811317170
CesarLeonC <51167786+CesarLeonC@users.noreply.github.com>
2021-08-09 13:33:40 -0500

    Update window-manager.sh

--- window-manager.sh
+++ window-manager.sh
@@ -4,3 +4,8 @@ sudo pacman -Syy
 # Install without conflicts 
 pacinstall --no-confirm --resolve-conflicts="all" xorg lightdm lightdm-gtk-greeter \
   xmonad xmonad-contrib xmobar dmenu picom nitrogen
+
+# Only on VM
+sed -i "216s/.//" /etc/xdg/picom.conf
+sed -i "216s/.//" /etc/xdg/picom.conf
+sed -e "217s/^/#/g" /etc/xdg/picom.conf
