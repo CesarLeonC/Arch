a3e02147068d52de8106274b9add22e2bc3820e3
CesarLeonC <51167786+CesarLeonC@users.noreply.github.com>
2021-08-09 11:14:18 -0500

    Update window-manager.sh

--- window-manager.sh
+++ window-manager.sh
@@ -1,2 +1,6 @@
 # Install Window Manager and Display Manager
-sudo pacman -Syy xorg lightdm lightdm-gtk-greeter xmonad xmonad-contrib xmobar dmenu picom nitrogen
+sudo pacman -Syy 
+
+# Install without conflicts 
+pacinstall --no-confirm --resolve-conflicts="all" xorg lightdm lightdm-gtk-greeter \
+  xmonad xmonad-contrib xmobar dmenu picom nitrogen
