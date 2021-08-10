89839f889f2a09d4bb8258c02c2240f0262a55b2
CesarLeonC <51167786+CesarLeonC@users.noreply.github.com>
2021-08-09 21:42:37 -0500

    Update install-gadgets.sh

--- install-gadgets.sh
+++ install-gadgets.sh
@@ -1,6 +1,22 @@
+# Install oh-my-zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" "" --unattended
+
+# Export personal config
+cat << EOF > /home/cesar/.zshrc
+export ZSH=$HOME/.oh-my-zsh
+ZSH_THEME="rkj-repos"
+DISABLE_UPDATE_PROMPT="true"
+export UPDATE_ZSH_DAYS=13
+plugins=(git github)
+source $ZSH/oh-my-zsh.sh
+if [[ -n $SSH_CONNECTION ]]; then
+  export EDITOR='vim'
+else
+  export EDITOR='nano'
+fi
+EOF
 
 # RUN THESE SCRIPTS AS ROOT
-# sudo -i
 # Include tap-to-click method on touchpads
 sudo cat << EOF > /etc/X11/xorg.conf.d/30-touchpad.conf
 Section "InputClass"
@@ -11,22 +27,5 @@ Section "InputClass"
 EndSection
 EOF
 
-# Install new shell, customizations, emacs and others
-sudo pacman -S --noconfirm zsh wget
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" "" --unattended
-usermod -s /bin/zsh cesar
-
-cat << EOF > /home/cesar/.zshrc
-export ZSH=$HOME/.oh-my-zsh
-ZSH_THEME="rkj-repos"
-DISABLE_UPDATE_PROMPT="true"
-export UPDATE_ZSH_DAYS=13
-plugins=(git github)
-
-source $ZSH/oh-my-zsh.sh
-if [[ -n $SSH_CONNECTION ]]; then
-  export EDITOR='vim'
-else
-  export EDITOR='nano'
-fi
-EOF
+# Changing default shell
+sudo usermod -s /bin/zsh cesar
