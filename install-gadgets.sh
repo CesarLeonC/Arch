e8d24823cdc4f7ae221de7c8085071471063e618
CesarLeonC <51167786+CesarLeonC@users.noreply.github.com>
2021-08-09 20:24:01 -0500

    Update install-gadgets.sh

--- install-gadgets.sh
+++ install-gadgets.sh
@@ -1,4 +1,4 @@
-# Corregir tapeo con click de los pads para mouse
+# Include tap-to-click method on touchpads
 cat << EOF > /etc/X11/xorg.conf.d/30-touchpad.conf
 Section "InputClass"
     Identifier "touchpad"
@@ -9,5 +9,21 @@ EndSection
 EOF
 
 # Install new shell, customizations, emacs and others
-sudo pacman -S zsh 
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+sudo pacman -S zsh wget
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" "" --unattended
+sud chsh -s $(which zsh)
+
+cat << EOF > /root/.zshrc
+export ZSH=$HOME/.oh-my-zsh
+ZSH_THEME="rkj-repos"
+DISABLE_UPDATE_PROMPT="true"
+export UPDATE_ZSH_DAYS=13
+plugins=(git github)
+
+source $ZSH/oh-my-zsh.sh
+if [[ -n $SSH_CONNECTION ]]; then
+  export EDITOR='vim'
+else
+  export EDITOR='nano'
+fi
+EOF
