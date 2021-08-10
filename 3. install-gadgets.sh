f6084fc5c3666fa4ed71c556bf5bc6f00345d91f
CesarLeonC <51167786+CesarLeonC@users.noreply.github.com>
2021-08-09 21:53:35 -0500

    Update and rename install-gadgets.sh to 3. install-gadgets.sh

--- 3. install-gadgets.sh
+++ 3. install-gadgets.sh
@@ -1,3 +1,13 @@
+#! /bin/bash
+
+#################################################
+#     Name: Cesar Leon C.
+#     Type: Shell Script
+#     Date: August, the 8th/ 2021
+#     Description: SCRIPT READY TO FIX PROBLEMS 
+#                  WITH MY PERSONAL PC
+#################################################
+
 # Install oh-my-zsh
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" "" --unattended
 
