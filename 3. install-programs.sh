18e6cc160806dc2103beb412c654fbb253b0fe8d
CesarLeonC <51167786+CesarLeonC@users.noreply.github.com>
2021-08-10 20:19:10 -0500

    Update 3. install-programs.sh

--- 3. install-programs.sh
+++ 3. install-programs.sh
@@ -13,7 +13,6 @@ sudo pacman -S --noconfirm fd emacs ripgrep
 # Install doom emacs
 git clone https://github.com/hlissner/doom-emacs /home/cesar/.emacs.d
 /home/cesar/.emacs.d/bin/doom install
-echo "export PATH=/home/cesar/.emacs.d/bin:$PATH
 
 # Check installation
 doom &>> ~/doom_log.txt
@@ -21,3 +20,14 @@ doom &>> ~/doom_log.txt
 # Install accesories
 sudo pacman -S --noconfirm zsh wget gimp obs \
           vifm audacity kdenlive vlc virtualbox
+
+# Export personal config
+cat << EOF > /home/cesar/.zshrc
+export PATH=/home/cesar/.emacs.d/bin:$PATH
+export ZSH=/home/cesar/.oh-my-zsh
+ZSH_THEME="rkj-repos"
+DISABLE_UPDATE_PROMPT="true"
+export UPDATE_ZSH_DAYS=13
+plugins=(git github)
+source /home/cesar/.oh-my-zsh/oh-my-zsh.sh
+EOF
