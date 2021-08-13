a8009a0c7ab92af01d019d39809dd68718b4b79e
CesarLeonC <51167786+CesarLeonC@users.noreply.github.com>
2021-08-12 22:23:40 -0500

    Update 3. install-programs.sh

--- 3. install-programs.sh
+++ 3. install-programs.sh
@@ -2,23 +2,26 @@
 
 #################################################
 #     Name: Cesar Leon C.
+#     E-mail: leoncesaralejandro@gmail.com
 #     Type: Shell Script
 #     Date: August, the 8th/ 2021
 #     Description: INSTALL VARIETY OF PROGRAMS
 #################################################
 
 # Actualizar los mirrors
-sudo reflector -c 'Ecuador,' -a 3 --sort rate --save /etc/pacman.d/mirrorlist
+sudo rm -rf /etc/pacman.d/mirrorlist
+sudo curl -o /etc/pacman.d/mirrorlist https://archlinux.org/mirrorlist/all/
+sudo sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist
 
 # Install emacs
 sudo pacman -S --noconfirm fd emacs ripgrep
 
 # Install doom emacs
-git clone https://github.com/hlissner/doom-emacs /home/cesar/.emacs.d
-/home/cesar/.emacs.d/bin/doom install
+git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
+~/.emacs.d/bin/doom install
 
 # Install accesories
-sudo pacman -S --noconfirm zsh wget gimp obs-studio \
-          vifm audacity kdenlive vlc virtualbox
+sudo pacman -S --noconfirm zsh wget obs-studio \
+          vifm vlc virtualbox
 # Change default user shell to zsh
 sudo usermod -s /bin/zsh cesar
