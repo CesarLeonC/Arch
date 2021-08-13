668ee3b0eb13ee66d793f6a08d716abd4163a4f6
CesarLeonC <51167786+CesarLeonC@users.noreply.github.com>
2021-08-13 00:17:13 -0500

    Update 4 install-gadgets.sh

--- 4 install-gadgets.sh
+++ 4 install-gadgets.sh
@@ -9,28 +9,6 @@
 #                  WITH MY PERSONAL PC
 #################################################
 
-# Export personal config
-cat << 'EOF' > /home/cesar/.zshrc
-###################################
-# 1. Exports
-###################################
-
-export PATH=~/.emacs.d/bin:$PATH
-export $XDG_CONFIG_HOME=~/.config/
-export ZSH=~/.oh-my-zsh
-
-export UPDATE_ZSH_DAYS=13
-
-###################################
-# 2. Variables
-###################################
-
-ZSH_THEME="rkj-repos"
-DISABLE_UPDATE_PROMPT="true"
-plugins=(git github)
-source $ZSH/oh-my-zsh.sh
-EOF
-
 # Recompile and Restart XMonad
 # (Mod+Shift+Q required to see changes)
 xmonad --recompile
