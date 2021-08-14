45376a6b9f69bf6241f8b64f0e71ef740207fbb0
CesarLeonC <51167786+CesarLeonC@users.noreply.github.com>
2021-08-13 20:20:39 -0500

    Update 2. window-manager.sh

--- 2. window-manager.sh
+++ 2. window-manager.sh
@@ -52,8 +52,8 @@ sudo pacman -S --noconfirm lightdm lightdm-gtk-greeter xmonad xmonad-contrib \
                 xmobar dmenu picom nitrogen alacritty chromium
 
 # Copiar el archivo de configuracion a la carpeta correspondiente
-cp ~/Repositories/cesarleonc//Arch/configs/xmonad.hs ~/.config/xmonad/xmonad.hs
-cp ~/Repositories/cesarleoncArch/configs/xmobarrc ~/.config/xmobar/xmobarrc
+cp ~/Repositories/cesarleonc/Arch/configs/xmonad.hs ~/.config/xmonad/xmonad.hs
+cp ~/Repositories/cesarleonc/Arch/configs/xmobarrc ~/.config/xmobar/xmobarrc
 
 sudo systemctl enable lightdm
 xmonad --recompile
