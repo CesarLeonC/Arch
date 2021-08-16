8d284ef4bbb52ac997a56cf00e7a7702d633edf6
CesarLeonC <51167786+CesarLeonC@users.noreply.github.com>
2021-08-16 10:13:54 -0500

    Update xmonad.hs

--- configs/xmonad.hs
+++ configs/xmonad.hs
@@ -1,41 +1,88 @@
-------------------------------------------
--- 1.- Imports
-------------------------------------------
+----------------------------------------------------------------
+--	Name:		Cesar Leon
+--	Type:		Haskell Script
+--	E-mail:		leoncesaralejandro@gmail.com
+--	Date:		August, the 15th/ 2021
+--	Description:	XMONAD CONFIGURATION FILE
+----------------------------------------------------------------
+
+----------------------------------------------------------------
+-- 1. Imports
+-- General modules used in the script
+-- (THIS SECTION SHOULD BE AT THE BEGINNING OF THE SCRIPT)
+----------------------------------------------------------------
 
 import XMonad
-import XMonad.Util.EZConfig
-import XMonad.Util.Ungrab
+
+import XMonad.Actions.MouseResize
+import XMonad.Actions.WindowArranger
+import XMonad.Actions.RotSlaves
+
 import XMonad.Layout.Grid
-import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.DynamicLog
-import XMonad.Config.Desktop
 
-------------------------------------------
--- 2.- Variables
-------------------------------------------
+import XMonad.Util.EZConfig
+import XMonad.Util.Run
+----------------------------------------------------------------
+-- 2. Variables
+-- Defining variables used in various hooks and configs
+----------------------------------------------------------------
 
-myBar         = xmobarPP {  }
-myModMask     = mod4Mask
-myTerminal    = "alacritty"
+myFont = "xft:"
+myModMask = mod4Mask
+myTerminal = "alacritty"
+myBrowser = "brave-browser"
+myIDE = "emacsclient -c -a 'emacs'"
 myBorderWidth = 2
-myLayoutHook  = grid ||| tiled ||| Mirror tiled ||| Full
-   where
-      grid    = Grid
-      tiled   = Tall nmaster delta ratio
-      nmaster = 1
-      ratio   = 1/2
-      delta   = 3/100
+myNormColor = "#282c34"
+myFocusColor = "#46d9ff"
+myWorkspaces = [" broken "," work "," rec "," ide "," vbox "]
 
-myConfig      = def
-   {
-   modMask        = myModMask,
-   terminal       = myTerminal,
-   layoutHook     = myLayoutHook
-   }
+----------------------------------------------------------------
+-- 3. Keybindings
+-- Custom shortcuts to optimize time around xmonad methods.
+-- Ordered alphabetically to ensure readability.
+-- Keybindings should relate the key with the method.
+----------------------------------------------------------------
 
-------------------------------------------
--- 3.- Main function
-------------------------------------------
+myKeybindings = [ ("M-t", spawn (myTerminal))			-- Open Terminal
+                , ("M-d", spawn "dmenu_run -i -p \"Verga: \"")	-- Open Dmenu
+                ]
+
+----------------------------------------------------------------
+-- 4. Hooks
+-- Using hooks to distinguish from configs in main function.
+-- 
+----------------------------------------------------------------
+
+-- Open apps at startup hook
+myStartupHook = do
+    spawnOnce "lxsession &"
+    spawnOnce "picom -f &"			-- compositor with fade effect
+    spawnOnce "nm-applet &"			-- network manager applet
+    spawnOnce "volumeicon &"			-- alsamixer volume icon
+    spawnOnce "/usr/bin/emacs --daemon &"	-- emacs daemon
+    spawnOnce "nitrogen --restore &"		-- wallpaper
+    setWMName "FirstWorkspace"			-- Name of Workspace
+
+myLayoutHook = mouseResize
+             $ windowArrange
+             $ Grid
+
+
+--myManageHooks = composeAll
+
+----------------------------------------------------------------
+-- 5. Configs
+----------------------------------------------------------------
 
 main :: IO ()
-main = xmonad . ewmh =<< myBar myConfig
+main = do
+       {
+       xbar0 <- spawnPipe "xmobar -x 0 $HOME/.config/xmobar/xmobarrc"
+       xmonad $ ewmh def
+           { modMask       = myModMask
+           , terminal      = myTerminal
+           , borderWidth   = myBorderWidth
+           , workspaces    = myWorkspaces
+           }
+       } `additionalKeysP` myKeybindings
