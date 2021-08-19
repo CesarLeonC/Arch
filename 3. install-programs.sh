74d7aefe708291ba6501871bc84716a3b2c4c906
CesarLeonC <51167786+CesarLeonC@users.noreply.github.com>
2021-08-19 15:37:50 -0500

    with Doom

--- 3. install-programs.sh
+++ 3. install-programs.sh
@@ -17,6 +17,25 @@ sudo pacman -S --noconfirm fd emacs ripgrep
 
 # Install doom emacs
 git clone https://github.com/hlissner/doom-emacs $XDG_CONFIG_HOME/emacs.d
+
+# So we don't have to write $XDG_CONFIG_HOME/emacs.d/bin/doom every time
+PATH="$XDG_CONFIG_HOME/emacs.d/bin:$PATH"
+
+# Create a directory for our private config
+mkdir $XDG_CONFIG_HOME/doom  # or ~/.config/doom
+
+# The init.example.el file contains an example doom! call, which tells Doom what
+# modules to load and in what order.
+cp $XDG_CONFIG_HOME/emacs.d/init.example.el $XDG_CONFIG_HOME/doom/init.el
+cp $XDG_CONFIG_HOME/emacs.d/core/templates/config.example.el $XDG_CONFIG_HOME/doom/config.el
+cp $XDG_CONFIG_HOME/emacs.d/core/templates/packages.example.el $XDG_CONFIG_HOME/doom/packages.el
+
+# Then synchronize Doom with your config:
+doom sync
+doom env
+
+# Lastly, install the icon fonts Doom uses:
+emacs --batch -f all-the-icons-install-fonts
 $XDG_CONFIG_HOME/emacs.d/bin/doom install
 
 # Install accesories
