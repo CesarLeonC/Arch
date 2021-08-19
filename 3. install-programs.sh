8d8f532bb0d3c544e0e0aa6f4d41ea577cade92c
CesarLeonC <51167786+CesarLeonC@users.noreply.github.com>
2021-08-19 00:24:55 -0500

    Update 3. install-programs.sh

--- 3. install-programs.sh
+++ 3. install-programs.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 
 #################################################
 #     Name: Cesar Leon C.
@@ -9,19 +9,19 @@
 #################################################
 
 # Actualizar los mirrors
-sudo rm -rf /etc/pacman.d/mirrorlist
-sudo curl -o /etc/pacman.d/mirrorlist https://archlinux.org/mirrorlist/all/
-sudo sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist
+sudo rm -rf $PACMAN_D_DIR/mirrorlist
+cp $XDG_CONFIG_HOME/mirrors/World-mirrorlist $PACMAN_D_DIR/mirrorlist
 
 # Install emacs
 sudo pacman -S --noconfirm fd emacs ripgrep
 
 # Install doom emacs
-git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
-~/.emacs.d/bin/doom install
+git clone https://github.com/hlissner/doom-emacs $XDG_CONFIG_HOME/emacs.d
+$XDG_CONFIG_HOME/emacs.d/bin/doom install
 
 # Install accesories
 sudo pacman -S --noconfirm obs-studio vifm vlc virtualbox
 
 # Return to Ecuadorian mirrorlist
-sudo reflector -c Ecuador -a 12 --sort rate --save /etc/pacman.d/mirrorlist
+sudo rm -rf $PACMAN_D_DIR/mirrorlist
+cp $XDG_CONFIG_HOME/mirrors/Ecuador-mirrorlist $PACMAN_D_DIR/mirrorlist
