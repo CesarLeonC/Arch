9771ac8d895192ebc8f3cfb8c1f591f2af538fc2
CesarLeonC <51167786+CesarLeonC@users.noreply.github.com>
2021-08-21 15:04:17 -0500

    xmonad

--- dotfiles/.config/xmonad/xmonad.hs
+++ dotfiles/.config/xmonad/xmonad.hs
@@ -51,9 +51,12 @@ myWorkspaces = [" broken "," work "," rec "," ide "," vbox "]
 ppWindow = xmobarRaw . (\w -> if null then "untitled" else w) . shorten 30
 
 -- Colors -------------------------------------------------------
-blue = xmobarColor #"bd93f9" ""
+
+blue = xmobarColor "#bd93f9" ""
 grey = xmobarColor "#808080" ""
+red = xmobarColor "#ff5555" ""
 white = xmobarColor "#f8f8f2" ""
+yellow = xmobarColor "#f1fa8c" ""
 
 ----------------------------------------------------------------
 -- 3. Keybindings
