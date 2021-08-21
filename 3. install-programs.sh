63c16101c38acc75c469e6ec19a9f597160b00b4
CesarLeonC <51167786+CesarLeonC@users.noreply.github.com>
2021-08-20 19:30:57 -0500

    fonts

--- 3. install-programs.sh
+++ 3. install-programs.sh
@@ -10,11 +10,9 @@
 
 # Install fonts
 ## Oficial Fonts
-sudo pacman -S \ 
-#        --noconfirm \
+sudo pacman -S --noconfirm
         noto-fonts noto-fonts-emoji \
         ttf-dejavu \
-#        texlive-core texlive-fontsextra \    # Pesa mucho
         otf-latin-modern otf-latinmodern-math
 ## AUR fonts
 git clone \
