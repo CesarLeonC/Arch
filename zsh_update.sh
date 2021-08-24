776032bdd17b0e783bdbc027ee4adae08ddffa0f
CesarLeonC <51167786+CesarLeonC@users.noreply.github.com>
2021-08-24 00:41:54 -0500

    changes

--- zsh_update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo pacman -Sy --noconfirm alacritty neovim pacman-mirrorlist zsh wget 
-cp ~/Repositories/cesarleonc/arch/dotfiles/.config/zsh/.zshenv ~/
-sudo usermod -s /bin/zsh cesar
-echo "Shell cambiado correctamente"
