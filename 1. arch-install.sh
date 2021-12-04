86aaf5996e0fec022e54c3816ea0b7e249ffc9ac
CesarLeonC <51167786+CesarLeonC@users.noreply.github.com>
2021-12-04 14:30:15 -0500

    Update 1. arch-install.sh

--- 1. arch-install.sh
+++ 1. arch-install.sh
@@ -22,53 +22,8 @@ locale-gen
 # Definir el lenguaje predeterminado
 echo "LANG=en_us.UTF-8" >> /etc/locale.conf
 # Definir el nombre de la maquina
-echo "master" >> /etc/hostname
+echo "cesarleonc" >> /etc/hostname
 # Crear los ip locales
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1 localhost" >> /etc/hosts
-echo "127.0.1.1 master.localdomain master" >> /etc/hosts
-
-pacman -S --noconfirm pacutils base-devel dnsutils
-
-# Cambiar el pass de root
-echo root:cesar.leon.14 | chpasswd
-
-# Instalar todo
-pacinstall --yolo grub networkmanager network-manager-applet dialog wpa_supplicant \
-                  mtools dosfstools reflector linux-headers avahi \
-                  xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils \
-                  bluez bluez-utils cups hplip alsa-utils pulseaudio bash-completion \
-                  openssh rsync acpi acpi_call tlp virt-manager dnsmasq \
-                  qemu qemu-arch-extra edk2-ovmf bridge-utils vde2 openbsd-netcat \
-                  iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid \
-                  os-prober ntfs-3g terminus-font os-prober broadcom-wl zsh neovim
-
-# Instalar grub dentro de BIOS/MBR
-grub-install --target=i386-pc /dev/sda
-# Habilitar el os-prober para encontrar Windows
-echo GRUB_DISABLE_OS_PROBER=false >> /etc/default/grub
-grub-mkconfig -o /boot/grub/grub.cfg
-
-# Habilitar servicios basicos
-systemctl enable NetworkManager
-systemctl enable bluetooth
-systemctl enable cups.service
-systemctl enable sshd
-systemctl enable avahi-daemon
-systemctl enable tlp
-systemctl enable reflector.timer
-systemctl enable fstrim.timer
-systemctl enable libvirtd
-systemctl enable firewalld
-systemctl enable acpid
-
-# Crear usuario local
-useradd -mG wheel cesar
-echo cesar:cesar.leon.14 | chpasswd
-usermod -aG libvirt cesar
-usermod -s /bin/zsh cesar
-cp ./arch/dotfiles/.config/zsh/.zshenv /home/cesar/
-
-echo "cesar ALL=(ALL) ALL" >> /etc/sudoers.d/cesar
-
-echo "Listo, proceso finalizado!"
+echo "127.0.1.1 cesarleonc.localdomain cesarleonc" >> /etc/hosts
